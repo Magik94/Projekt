72a8a5a92aa16331760139f8ab47c7e7aac96632
”Dawid <dedly04@gmail.com>
2017-10-22 15:18:07 +0200

    proba

--- src/main/java/com/mkyong/SpringBootWebApplication.java
+++ src/main/java/com/mkyong/SpringBootWebApplication.java
@@ -14,7 +14,6 @@ public class SpringBootWebApplication extends SpringBootServletInitializer {
 	}
 
 	public static void main(String[] args) throws Exception {
-
 		SpringApplication.run(SpringBootWebApplication.class, args);
 	}
 
